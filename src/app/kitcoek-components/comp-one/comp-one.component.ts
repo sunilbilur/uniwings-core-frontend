0616a2252b070f8429e5d180325430cd05627abe
Sunil Bilur <144220583+sunilbilur@users.noreply.github.com>
2024-04-30 19:00:29 +0530

    set up primeng component kit; import styles in angular.json

--- src/app/kitcoek-components/comp-one/comp-one.component.ts
+++ src/app/kitcoek-components/comp-one/comp-one.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-comp-one',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './comp-one.component.html',
   styleUrl: './comp-one.component.css'
 })
